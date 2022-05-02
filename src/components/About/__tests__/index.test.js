5f2620183914b66eb648ec511509bd49f83c8b38
JoDamaso <darryldamaso@gmail.com>
2022-05-02 18:44:45 -0500

    About comp tested

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+//We need react to enable the components to function properly so we can test them.
+import { render, cleanup } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+//import the component we're testing
+import About from "..";
+
+afterEach(cleanup);
+// This will ensure that after each test, we won't have any leftover memory data that could give us false results.
+
+// Then we'll use the describe function to declare the component we're testing, by adding the following code:
+describe('About component', () => {
+
+    //first test
+    // it function
+    // string that declares what is being tested
+    it ('renders', () => {
+        //call back function that runs the test
+        render(<About />);
+    });
+
+    //second testing
+    it ('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
